11602b81787f9748fa3cada4a1651ffc4628cfa1
vikawu35 <122668662+vikawu35@users.noreply.github.com>
2023-08-26 18:03:05 +0900

    Update Goat6.java

--- src/main/java/feast/models/Goat6.java
+++ src/main/java/feast/models/Goat6.java
@@ -8,16 +8,13 @@ public class Goat6 {
 
     private Horse7 horse7;
 
-    public Goat6() {
-
-    }
-    @Autowired
-    public void setHorse7(Horse7 horse7) {
-        this.horse7 = horse7;
-    }
-
     @Override
     public String toString() {
         return "which is stuffed inside a horse, " + horse7.toString();
+    }
+
+    @Autowired
+    public void setHorse7(Horse7 horse7) {
+        this.horse7 = horse7;
     }
 }
